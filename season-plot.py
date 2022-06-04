3ab566a0067b0d29f19ef797b02d9599ee66fa33
David DiBenedetto <david@njweb.solutions>
2022-06-04 18:08:17 -0400

    update

--- season-plot.py
+++ season-plot.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 years = {}
 
 with open(sys.argv[1], 'r') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		year = int(row[0][:4])
-		close = float(row[5])
-		if year in years:
-			years[year].append(close)
-		else:
-			years[year] = [close]
+    reader = csv.reader(f)
+    for row in reader:
+        original_close = float(row[5])
+        break
+    for row in reader:
+        year = int(row[0][:4])
+        close = float(row[5])
+        if year in years:
+            years[year].append(close/original_close)
+        else:
+            years[year] = [1]
+            original_close = float(row[5])
 for year in years:
-	plt.plot(years[year], label=year)
+    plt.plot(years[year], label=year)
 plt.legend()
 plt.show()
